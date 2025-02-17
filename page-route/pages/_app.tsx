a75fd12e4521c4c8ca9378817440e733bc967753
fahrillrizal <arilcoy14@gmail.com>
2025-02-17 22:48:41 +0700

    .

---
+++ page-route/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import RootLayout from "@/layout";
+import { useRouter } from "next/router";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const metaTitle =
+    router.pathname === "/" ? "homepage" : router.pathname.replace("/", "");
+  return (
+    <RootLayout metaTitle={metaTitle}>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+}
