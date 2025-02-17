68e6c8d6aaec2df2a7a131625abee6fc5d8cd854
fahrillrizal <arilcoy14@gmail.com>
2025-02-17 23:00:35 +0700

    add tugas 6

---
+++ page-route/pages/api/user/[id].ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+    name?: string
+    id?: string | string[] | undefined
+    message?: string
+    data?: object
+    headers?: string | string[] | undefined
+};
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>,
+) {
+    if (req.method === "POST") {
+        res.status(200).json({ id: req?.query?.id, name: "John Doe", data: req.body, headers: req.headers['api-token'] });
+    }else {
+        res.status(403).json({ message: "Forbidden" })
+    }
+}
